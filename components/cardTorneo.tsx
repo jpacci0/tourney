9e8c2c9ff767813343bd631c047e350aadbffc43
Jacopo Paccioretti <pacciorettijacopo@gmail.com>
2024-02-24 18:28:33 +0100

    edit: modifica ui cardtorneo, z-index nav messa a 50 da che non c'era e cardtorneo a 10

--- components/cardTorneo.tsx
+++ components/cardTorneo.tsx
@@ -31,112 +31,124 @@ export default function CardTorneo({ tournament }: cardTorneoProps) {
     <>
       {tournament.status === "done" ? (
         <Link href={`/fine-torneo?id=${tournament.idclient}&tab=leaderboard`}>
-          <div className="rounded-md shadow-xl hover:shadow-xl hover:shadow-orange-500/10 text-gray-200 h-[324px] bg-primary px-4 py-8">
-            {/* <Image
+          <div
+            className="relative overflow-hidden rounded-md shadow-xl hover:shadow-xl hover:shadow-orange-500/10
+          p-1"
+          >
+            <div className="absolute inset-0 bg-gradient-to-r from-gray-500 to-gray-800 p-4 animate-pulse"></div>
+            <div className="relative z-10 h-[324px] bg-primary px-4 py-8 rounded-md text-gray-200">
+              {/* <Image
           src={wz}
           width={1000}
           height={100}
           alt="wz"
           className="rounded-lg"
         /> */}
-            <h2
-              className={`w-full mb-5 text-xl text-orange-500 font-bold, ${BlackOpsOne.className}`}
-            >
-              {tournament.name}
-            </h2>
+              <h2
+                className={`w-full mb-5 text-xl text-orange-500 font-bold, ${BlackOpsOne.className}`}
+              >
+                {tournament.name}
+              </h2>
 
-            <div className="flex gap-2 col-span-2">
-              <Clock9 className="text-gray-500" />
-              {tournament.start_time.replace("T", " ")}
-            </div>
-            <div className="flex gap-2 col-span-2 mt-3">
-              <Gamepad2 className="text-gray-500" />
-              <p>{tournament.platform}</p>
-            </div>
-            <div className="grid grid-cols-3 gap-3 mt-4">
-              <div>
-                <p className="text-gray-500">Map</p>
-                <p>{tournament.map}</p>
+              <div className="flex gap-2 col-span-2">
+                <Clock9 className="text-gray-500" />
+                {tournament.start_time.replace("T", " ")}
               </div>
-              <div>
-                <p className="text-gray-500">Game mode</p>
-                <p>{tournament.game_mode}</p>
+              <div className="flex gap-2 col-span-2 mt-3">
+                <Gamepad2 className="text-gray-500" />
+                <p>{tournament.platform}</p>
               </div>
-              <div>
-                <p className="text-gray-500">Rounds</p>
-                <p>{tournament.rounds}</p>
+              <div className="grid grid-cols-3 gap-3 mt-4">
+                <div>
+                  <p className="text-gray-500">Map</p>
+                  <p>{tournament.map}</p>
+                </div>
+                <div>
+                  <p className="text-gray-500">Game mode</p>
+                  <p>{tournament.game_mode}</p>
+                </div>
+                <div>
+                  <p className="text-gray-500">Rounds</p>
+                  <p>{tournament.rounds}</p>
+                </div>
               </div>
-            </div>
-            <div className="grid grid-cols-2 gap-3 mt-4">
-              <div>
-                <p className="text-gray-500">Price per team</p>
-                {tournament.registration_price === 0 ? (
-                  <div className="flex gap-1">
-                    <Euro className="text-green-500" />
-                    <p>Free entry</p>
-                  </div>
-                ) : (
-                  <div className="flex gap-1">
-                    <Euro className="text-green-500" />
-                    <p>{tournament.registration_price}</p>
-                  </div>
-                )}
+              <div className="grid grid-cols-2 gap-3 mt-4">
+                <div>
+                  <p className="text-gray-500">Price per team</p>
+                  {tournament.registration_price === 0 ? (
+                    <div className="flex gap-1">
+                      <Euro className="text-green-500" />
+                      <p>Free entry</p>
+                    </div>
+                  ) : (
+                    <div className="flex gap-1">
+                      <Euro className="text-green-500" />
+                      <p>{tournament.registration_price}</p>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
         </Link>
       ) : (
         <Link href={`/tournament?id=${tournament.idclient}&tab=overview`}>
-          <div className="rounded-md shadow-xl hover:shadow-xl hover:shadow-orange-500/10 text-gray-200 h-[324px] bg-primary px-4 py-8">
-            {/* <Image
+          <div
+            className="relative overflow-hidden rounded-md shadow-2xl hover:shadow-2xl hover:shadow-gray-500/20
+          p-1"
+          >
+            <div className="absolute inset-0 bg-gradient-to-r from-gray-500 to-gray-800 p-4 animate-pulse"></div>
+            <div className="relative z-10 h-[324px] bg-primary px-4 py-8 rounded-md text-gray-200">
+              {/* <Image
           src={wz}
           width={1000}
           height={100}
           alt="wz"
           className="rounded-lg"
         /> */}
-            <h2
-              className={`w-full mb-5 text-xl text-orange-500 font-bold, ${BlackOpsOne.className}`}
-            >
-              {tournament.name}
-            </h2>
+              <h2
+                className={`w-full mb-5 text-xl text-orange-500 font-bold, ${BlackOpsOne.className}`}
+              >
+                {tournament.name}
+              </h2>
 
-            <div className="flex gap-2 col-span-2">
-              <Clock9 className="text-gray-500" />
-              {tournament.start_time.replace("T", " ")}
-            </div>
-            <div className="flex gap-2 col-span-2 mt-3">
-              <Gamepad2 className="text-gray-500" />
-              <p>{tournament.platform}</p>
-            </div>
-            <div className="grid grid-cols-3 gap-3 mt-4">
-              <div>
-                <p className="text-gray-500">Map</p>
-                <p>{tournament.map}</p>
+              <div className="flex gap-2 col-span-2">
+                <Clock9 className="text-gray-500" />
+                {tournament.start_time.replace("T", " ")}
               </div>
-              <div>
-                <p className="text-gray-500">Game mode</p>
-                <p>{tournament.game_mode}</p>
+              <div className="flex gap-2 col-span-2 mt-3">
+                <Gamepad2 className="text-gray-500" />
+                <p>{tournament.platform}</p>
               </div>
-              <div>
-                <p className="text-gray-500">Rounds</p>
-                <p>{tournament.rounds}</p>
+              <div className="grid grid-cols-3 gap-3 mt-4">
+                <div>
+                  <p className="text-gray-500">Map</p>
+                  <p>{tournament.map}</p>
+                </div>
+                <div>
+                  <p className="text-gray-500">Game mode</p>
+                  <p>{tournament.game_mode}</p>
+                </div>
+                <div>
+                  <p className="text-gray-500">Rounds</p>
+                  <p>{tournament.rounds}</p>
+                </div>
               </div>
-            </div>
-            <div className="grid grid-cols-2 gap-3 mt-4">
-              <div>
-                <p className="text-gray-500">Price per team</p>
-                {tournament.registration_price === 0 ? (
-                  <div className="flex gap-1">
-                    <Euro className="text-green-500" />
-                    <p>Free entry</p>
-                  </div>
-                ) : (
-                  <div className="flex gap-1">
-                    <Euro className="text-green-500" />
-                    <p>{tournament.registration_price}</p>
-                  </div>
-                )}
+              <div className="grid grid-cols-2 gap-3 mt-4">
+                <div>
+                  <p className="text-gray-500">Price per team</p>
+                  {tournament.registration_price === 0 ? (
+                    <div className="flex gap-1">
+                      <Euro className="text-green-500" />
+                      <p>Free entry</p>
+                    </div>
+                  ) : (
+                    <div className="flex gap-1">
+                      <Euro className="text-green-500" />
+                      <p>{tournament.registration_price}</p>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -5,7 +5,7 @@ import AuthButton from "@/components/ui/authButton";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center border-b-2 mx-2 md:mx-4 xl:mx-10 2xl:mx-48 fixed top-0 left-0 right-0 bg-gray-950">
+    <nav className="flex justify-between items-center border-b-2 mx-2 md:mx-4 xl:mx-10 2xl:mx-48 fixed top-0 left-0 right-0 z-50 bg-gray-950">
       <div className="">
         <Link href="/" className="mx-auto">
           <Image src={lifelogo} width={80} height={80} alt="Logo Life" />
