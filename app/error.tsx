dbcda62a364b389aa87dcf4846b75deedaca4cc4
Jacopo Paccioretti <pacciorettijacopo@gmail.com>
2024-02-25 18:41:36 +0100

    edit: error page, gestione errore

--- app/error.tsx
+++ app/error.tsx
@@ -42,15 +42,6 @@ export default function Error({
           .
         </p>
       </section>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
     </main>
   );
 }
--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,6 +1,3 @@
-"use client"; // Error components must be Client Components
-
-import { useEffect } from "react";
 import Header from "@/components/header";
 import SubHeader from "@/components/subHeader";
 import { Separator } from "@/components/ui/separator";
@@ -8,18 +5,7 @@ import errorImage from "@/public/error.jpeg";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
-  // useEffect(() => {
-  //   // Log the error to an error reporting service
-  //   console.error(error);
-  // }, [error]);
-
+export default function ErrorPage() {
   return (
     <main>
       <Header>Error</Header>
@@ -42,15 +28,6 @@ export default function Error({
           .
         </p>
       </section>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
     </main>
   );
 }
